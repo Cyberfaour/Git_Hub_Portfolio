00aa1d20cdd66157b2f0e16fc94c63f2c4750e18
Cyberfaour <ali2001faour@outlook.com>
2023-06-11 19:00:07 +0400

    update7

--- scripts.js
+++ scripts.js
@@ -1,51 +1,64 @@
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function acceptCookies() {
-  setCookie("cookieConsent", "true", 365);
-  document.getElementById("cookieBanner").style.display = "none";
-}
-
-function rejectCookies() {
-  setCookie("cookieConsent", "false", 365);
-  document.getElementById("cookieBanner").style.display = "none";
-}
-
-function checkCookieConsent() {
-  var cookieConsent = getCookie("cookieConsent");
-  if (cookieConsent) {
-    document.getElementById("cookieBanner").style.display = "none";
-  }
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
-checkCookieConsent();
-
-function updateViewCount() {
-  var countElement = document.getElementById("count");
-  if (localStorage.viewCount) {
-    localStorage.viewCount = Number(localStorage.viewCount) + 1;
-  } else {
-    localStorage.viewCount = 1;
-  }
-  countElement.textContent = localStorage.viewCount;
-}
-
-updateViewCount();
+document.addEventListener('DOMContentLoaded', function() {
+    var acceptButton = document.getElementById('acceptButton');
+    var rejectButton = document.getElementById('rejectButton');
+  
+    acceptButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default form submission
+      acceptCookies();
+    });
+  
+    rejectButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default form submission
+      rejectCookies();
+    });
+  
+    function acceptCookies() {
+      createCookie('cookieConsent', 'true', 365); // Create a cookie named 'cookieConsent' with the value 'true' that expires in 365 days
+      hideCookieBanner();
+    }
+  
+    function rejectCookies() {
+      createCookie('cookieConsent', 'false', 365); // Create a cookie named 'cookieConsent' with the value 'false' that expires in 365 days
+      hideCookieBanner();
+    }
+  
+    function createCookie(name, value, days) {
+      var expires = '';
+  
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+      }
+  
+      var cookieString = name + '=' + value + expires + '; path=/';
+  
+      // Set the Secure attribute if the page is served over HTTPS
+      if (window.location.protocol === 'https:') {
+        cookieString += '; Secure';
+      }
+  
+      document.cookie = cookieString;
+    }
+  
+    function hideCookieBanner() {
+        var banner = document.getElementById('cookieBanner');
+        banner.classList.add('hide');
+        setTimeout(function() {
+          banner.style.display = 'none';
+        }, 500);
+    }
+         // Hide the banner after the transition duration (500ms)    }
+  
+    var cookieConsent = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('cookieConsent='))
+      ?.split('=')[1];
+  
+    if (cookieConsent === 'true') {
+      hideCookieBanner();
+    } else {
+      document.getElementById('cookieBanner').style.display = 'block';
+    }
+  });
+  
